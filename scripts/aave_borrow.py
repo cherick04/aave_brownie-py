b85daaca22fb3a37f45680a21ab5c98cfa7c240c
Erick C <cherick04@hotmail.com>
2021-12-28 17:23:02 -0300

    Repaying debt

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -47,6 +47,10 @@ def main():
     print("We borrowed some DAI!")
     print("New data:")
     get_borrowable_data(lending_pool, account)
+    # Repay
+    print("Repaying...")
+    repay_all(amount, lending_pool, account)
+    print("You just deposited, borrowed and repayed with Aave, Brownie and Chainlink!")
 
 
 def get_lending_pool():
@@ -90,3 +94,21 @@ def get_asset_price(dai_eth_price_feed):
     latest_price = Web3.fromWei(dai_eth_price_feed.latestRoundData()[1], "ether")
     print(f"The latest DAI/ETH price is {latest_price}")
     return float(latest_price)
+
+
+def repay_all(amount, lending_pool, account):
+    approve_erc20(
+        Web3.toWei(amount, "ether"),
+        lending_pool,
+        config["networks"][network.show_active()]["dai_token"],
+        account,
+    )
+    repay_tx = lending_pool.repay(
+        config["networks"][network.show_active()]["dai_token"],
+        amount,
+        1,
+        account.address,
+        {"from": account},
+    )
+    repay_tx.wait(1)
+    print("Repaid!")
